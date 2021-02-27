ea56bf569a7cb737370a79b02b0627bb84875ec8
Ashish Karki <4175758+ashishkarki@users.noreply.github.com>
2021-02-27 22:02:58 +0545

    Implemented shopping-cart project using useReducer and useContext hooks with hardcoded data

---
+++ shopping-cart/src/context.js
@@ -0,0 +1,70 @@
+import React, { useState, useContext, useReducer, useEffect } from 'react'
+import cartItems from './data'
+import reducer, { ACTION_TYPES } from './reducer'
+
+const url = 'https://course-api.com/react-useReducer-cart-project'
+const AppContext = React.createContext()
+
+const initialState = {
+    loading: false,
+    cart: cartItems,
+    totalAmount: 0,
+    numItems: 0,
+}
+
+const AppProvider = ({ children }) => {
+    const [ state, dispatch ] = useReducer(reducer, initialState)
+
+    const clearCart = () => {
+        dispatch({
+            type: ACTION_TYPES.CLEAR_CART
+        })
+    }
+
+    const removeOneItem = (id) => {
+        dispatch({
+            type: ACTION_TYPES.REMOVE_ITEM,
+            payload: id,
+        })
+    }
+
+    const increaseItems = (id) => {
+        dispatch({
+            type: ACTION_TYPES.INCREASE_ITEM_COUNT,
+            payload: id,
+        })
+    }
+
+    const decreaseItems = (id) => {
+        dispatch({
+            type: ACTION_TYPES.DECREASE_ITEM_COUNT,
+            payload: id,
+        })
+    }
+
+    useEffect(() => {
+        dispatch({
+            type: ACTION_TYPES.GET_TOTAL_ITEMS
+        })
+    }, [ state.cart ])
+
+    return (
+        <AppContext.Provider
+            value={ {
+                ...state,
+                clearCart,
+                removeOneItem,
+                increaseItems,
+                decreaseItems,
+            } }
+        >
+            {children }
+        </AppContext.Provider>
+    )
+}
+
+export const useGlobalContext = () => {
+    return useContext(AppContext)
+}
+
+export { AppContext, AppProvider }
---
+++ shopping-cart/src/reducer.js
@@ -0,0 +1,86 @@
+
+export const ACTION_TYPES = {
+    CLEAR_CART: 'clear whole cart',
+    REMOVE_ITEM: 'remove specific item',
+    INCREASE_ITEM_COUNT: 'increase count of an item',
+    DECREASE_ITEM_COUNT: 'decrease count of an item',
+    GET_TOTAL_ITEMS: 'get a count of total items',
+}
+
+const reducer = (state, action) => {
+
+    if (action.type === ACTION_TYPES.CLEAR_CART) {
+        return {
+            ...state,
+            cart: [],
+        }
+    } else if (action.type === ACTION_TYPES.REMOVE_ITEM) {
+        const updatedCart = state.cart.filter(item =>
+            item.id !== action.payload
+        )
+
+        return {
+            ...state,
+            cart: updatedCart,
+        }
+    } else if (action.type === ACTION_TYPES.INCREASE_ITEM_COUNT) {
+        const updatedCart = _updateCart(state, action.payload)
+
+        return {
+            ...state,
+            cart: updatedCart,
+        }
+    } else if (action.type === ACTION_TYPES.DECREASE_ITEM_COUNT) {
+        const updatedCart = _updateCart(state, action.payload, false)
+            .filter(cartItem => cartItem.numItems !== 0)
+
+        return {
+            ...state,
+            cart: updatedCart,
+        }
+    } else if (action.type === ACTION_TYPES.GET_TOTAL_ITEMS) {
+        let { totalAmount, numItems } = state.cart.reduce((
+            cartTotal, cartItem,
+        ) => {
+            // reduce(function(total, currentValue, currentIndex, arr), initialValue)
+            const { price, numItems } = cartItem
+            const itemTotal = price * numItems
+
+            cartTotal.totalAmount += itemTotal
+            cartTotal.numItems += numItems
+
+            return cartTotal
+        }, { // initial value
+            totalAmount: 0,
+            numItems: 0,
+        })
+
+        totalAmount = parseFloat(totalAmount.toFixed(2))
+
+        return {
+            ...state,
+            totalAmount, // totalAmount: totalAmount,
+            numItems, // numItems: numItems,
+        }
+    }
+
+
+    return state
+}
+
+const _updateCart = (state, id, isIncrement = true) => {
+    return state.cart.map(cartItem => {
+        if (cartItem.id === id) {
+            return {
+                ...cartItem,
+                numItems: isIncrement ?
+                    cartItem.numItems + 1 :
+                    cartItem.numItems - 1,
+            }
+        }
+
+        return cartItem
+    })
+}
+
+export default reducer
