b9d5158e2658be05b7855e9de8bc036e5c0beeae
Ashish Karki <4175758+ashishkarki@users.noreply.github.com>
2021-03-16 09:49:09 +0545

    finished implementing cocktails-db project.

--- cocktails-db/src/components/Loading.js
+++ cocktails-db/src/components/Loading.js
@@ -1,10 +1,37 @@
+// import React from 'react'
+
+// const Loading = () => {
+//  return (
+//   <div className="loader">
+//   </div>
+//  )
+// }
+
+// export default Loading
+
 import React from 'react'
+import { makeStyles } from '@material-ui/core/styles'
+import CircularProgress from '@material-ui/core/CircularProgress'
 
-const Loading = () => {
- return (
-  <div className="loader">
-  </div>
- )
+const useStyles = makeStyles((theme) => ({
+    root: {
+        display: 'flex',
+        '& > * + *': {
+            marginLeft: theme.spacing(2),
+        },
+    },
+}))
+
+export default function Loading() {
+    const classes = useStyles()
+
+    return (
+        <div className={ classes.root }
+            style={
+                { margin: '25% 40%' }
+            }>
+            <CircularProgress />
+            <CircularProgress color="secondary" />
+        </div>
+    )
 }
-
-export default Loading
